c7f52a895d6be14d705f1e361c07771c7f5eb3c1
Lê Trần Bảo Cương <cuongltb@kalapa.vn>
2023-03-01 15:50:56 +0700

    minor changes

--- apix/consumer/sidebars.ts
+++ apix/consumer/sidebars.ts
@@ -3,47 +3,11 @@ module.exports = {
     'introduction',
     {
       type: 'category',
-      label: 'Marketplace',
-      link: {
-        type: 'doc',
-        id: 'api/marketplace/general',
-      },
+      label: 'Usage',
       collapsed: false,
       items: [
-        {
-          type: 'category',
-          label: 'API',
-          collapsed: false,
-
-          items: ['api/marketplace/general'],
-        },
-        {
-          type: 'category',
-          label: 'Dataset',
-          collapsed: false,
-          items: ['dataset/marketplace/general'],
-        },
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Account Management',
-      collapsed: false,
-      items: [
-        {
-          type: 'category',
-          label: 'API',
-          collapsed: false,
-          items: ['api/dashboard/general'],
-        },
-        {
-          type: 'category',
-          label: 'Dataset',
-          collapsed: false,
-          items: ['dataset/dashboard/general'],
-        },
-        'balance',
-        'transaction-history',
+        'product/api',
+        'product/dataset',
       ],
     },
   ],
